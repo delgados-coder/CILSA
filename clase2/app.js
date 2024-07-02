97b62ac95a1cdafa775ef5616b728a5f9fc8c7b7
Santi Delgado <santid.prog@gmail.com>
2024-07-01 22:40:05 -0300

    Clase2
    Se agrego la clase 2 - tarea2

---
+++ clase2/app.js
@@ -0,0 +1,7 @@
+function estilo_normal() {
+    document.getElementById('archivo_css').href = './normal.css';
+}
+
+function estilo_alto_contraste() {
+    document.getElementById('archivo_css').href = './alto.css';
+}
