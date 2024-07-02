e63dd46ccffa16aa9ea228f9920565388c707314
Santi Delgado <santid.prog@gmail.com>
2024-07-01 21:33:25 -0300

    Ejercicios Subidos
    Se presentan los ejercicios pedidos

---
+++ clase1/ej2/ej2.js
@@ -0,0 +1,7 @@
+function estilo_normal() {
+    document.getElementById('archivo_css').href = '../ej1/ej1.css';
+}
+
+function estilo_alto_contraste() {
+    document.getElementById('archivo_css').href = './ej2.css';
+}
